6a5a0415d578aa3337c458574e890691cfe7f53d
BelenMaru <bleneee@gmail.com>
2021-09-14 01:50:14 +0000

    Add full outerjoin to sql

--- 1_queries/1-students-cohorts_nomatch.sql
+++ 1_queries/1-students-cohorts_nomatch.sql
@@ -1,2 +1,2 @@
 SELECT students.name as student_name, email, cohorts.name as cohort_name
-FROM students LEFT OUTER JOIN cohorts ON cohorts.id = cohort_id;
+FROM students FULL OUTER JOIN cohorts ON cohorts.id = cohort_id;
